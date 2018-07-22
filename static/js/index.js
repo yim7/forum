b36ece257f3c6986b4d56057ff31273a0ca5c1ee
yim <yimchiu7@gmail.com>
2018-07-22 20:59:29 +0800

    优化帖子和回复时间显示

--- static/js/index.js
+++ static/js/index.js
@@ -24,19 +24,41 @@ var highlight = function () {
     // 自动加载对应的语言 不然要手动加入各个语言的 js
     Prism.plugins.autoloader.languages_path = 'https://cdn.bootcss.com/prism/1.13.0/components/'
 }
-
-var registerTimer = function () {
-    setInterval(function () {
-        var times = es('.created-time')
-        for (var i = 0; i < times.length; i++) {
-            var t = times[i]
-            var time = Number(t.dataset.created_time)
-            var now = Math.floor(new Date() / 1000)
-            var delta = now - time
-            var s = `${delta} 秒前`
-            t.innerText = s
+var update_time = function () {
+    var times = es('.created-time')
+    for (var i = 0; i < times.length; i++) {
+        var t = times[i]
+        var time = Number(t.dataset.created_time)
+        var now = Math.floor(new Date() / 1000)
+        var delta = now - time
+        var s = ''
+        if (delta > 31536000) {
+            year = Math.floor(delta / 31536000)
+            s = ` ${year} 年前`
+        } else if (delta > 2592000) {
+            year = Math.floor(delta / 2592000)
+            s = ` ${year} 个月前`
+        } else if (delta > 2592000) {
+            year = Math.floor(delta / 2592000)
+            s = ` ${year} 个月前`
+        } else if (delta > 84600) {
+            year = Math.floor(delta / 86400)
+            s = ` ${year} 天前`
+        } else if (delta > 3600) {
+            year = Math.floor(delta / 3600)
+            s = ` ${year} 小时前`
+        } else if (delta > 60) {
+            year = Math.floor(delta / 60)
+            s = ` ${year} 分钟前`
+        } else {
+            s = ` 几秒前`
         }
-    }, 1000)
+
+        t.innerText = s
+    }
+}
+var registerTimer = function () {
+    setInterval(update_time, 1000)
 }
 
 function footer() {
@@ -48,6 +70,7 @@ $(document).ready(function () {
 })
 
 var __main = function () {
+    update_time()
     markContents()
     highlight()
     registerTimer()
