bbae4806299db9aac4222d57cb776fcea41bfca2
yim <yimchiu7@gmail.com>
2018-07-26 10:49:36 +0800

    完善后台删除功能，删除用户同时删除主题和回复，并更新缓存

--- routes/admin.py
+++ routes/admin.py
@@ -8,11 +8,14 @@ from models.topic import Topic
 from models.reply import Reply
 from models.board import Board
 
+from models.cache import update_created_topic_cache, update_replied_topic_cache
+
 from routes import current_user, login_required
 
 
 # Create customized model view class
-class MyModelView(ModelView):
+class BaseModelView(ModelView):
+    column_display_pk = True
 
     def is_accessible(self):
         u = current_user()
@@ -20,6 +23,36 @@ class MyModelView(ModelView):
             return u.is_admin()
         else:
             return False
+
+
+class UserModelView(BaseModelView):
+
+    def delete_model(self, model):
+        ts = Topic.all(user_id=model.id)
+        for t in ts:
+            Topic.delete(t.id)
+        Reply.delete(user_id=model.id)
+        User.delete(id=model.id)
+        return True
+
+
+class TopicModelView(BaseModelView):
+
+    def delete_model(self, model):
+        # 先保存 replier id，Topic删除后就拿不到了
+        rs = model.replies()
+        rs_id = set([r.user_id for r in rs])
+
+        Topic.delete(id=model.id)
+        update_created_topic_cache(user_id=model.user_id)
+
+        for _id in rs_id:
+            update_replied_topic_cache(user_id=_id)
+        return True
+
+
+class BoardModelView(BaseModelView):
+    can_delete = False
 
 
 # Create customized index view class that handles login & registration
@@ -35,7 +68,7 @@ class MyAdminIndexView(AdminIndexView):
 
 admin = Admin(name='论坛后台', index_view=MyAdminIndexView(), template_mode='bootstrap3')
 
-admin.add_view(MyModelView(User, db.session, name='用户', endpoint='用户'))
-admin.add_view(MyModelView(Topic, db.session, name='主题', endpoint='主题'))
-admin.add_view(MyModelView(Reply, db.session, name='评论', endpoint='评论'))
-admin.add_view(MyModelView(Board, db.session, name='板块', endpoint='板块'))
+admin.add_view(UserModelView(User, db.session, name='用户', endpoint='用户'))
+admin.add_view(TopicModelView(Topic, db.session, name='主题', endpoint='主题'))
+admin.add_view(BaseModelView(Reply, db.session, name='评论', endpoint='评论'))
+admin.add_view(BoardModelView(Board, db.session, name='板块', endpoint='板块'))
